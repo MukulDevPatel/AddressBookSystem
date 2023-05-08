472450b9a44123515b4bf773f73b6301830fc680
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-08 17:51:38 +0530

    [Mukul Dev] Add: Create contact in Address Book program

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -1,10 +1,27 @@
-﻿namespace AddressBookSystem
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
 {
-    class AddressBookMain
+    public class AddressBookMain
     {
-        public static void Main(string[] args)
+        public static void AddDetails()
         {
-            Console.WriteLine("Welcome to Address Book program");
+            Contact contact = new Contact()
+            {
+                FirstName = "Mukul",
+                LastName = "Patel",
+                Address = "Khopli",
+                City = "Durg",
+                State = "Chhattisgarh",
+                Zip = 123456,
+                PhoneNumber = 1234567891,
+                Email = "patelmukulDev@gamil.com"
+            };
+            Console.WriteLine(contact.FirstName +"\n" +contact.LastName + "\n"+ contact.Address + "\n"+ contact.City + "\n"+ contact.State + "\n"+ contact.Zip + "\n"+ contact.PhoneNumber + "\n"+ contact.Email);
         }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AddressBookSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program");
+            AddressBookMain.AddDetails();
+            
+        }
+    }
+}
