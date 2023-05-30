151dba5d5edbb9f96dfbf29ead3d55c03b989b04
MukulDevPatel <patelmukuldev@gmail.com>
2023-05-30 22:26:32 +0530

    [Mukul Dev] Add: This program find person in address book by their city or state name using Dictionary

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -9,10 +9,26 @@ namespace AddressBookSystem
 {
     public class AddressBookMain
     {
-        private List<Contact> people; 
+        private List<Contact> people;
+        private Dictionary<String, List<Contact>> contactByCity;
+        private Dictionary<String, List<Contact>> contactByState;
         public AddressBookMain()
         {
             people = new List<Contact>();
+            contactByCity = new Dictionary<String, List<Contact>>();
+            contactByState = new Dictionary<String, List<Contact>>();
+        }
+
+        //Add persons to Dictionary
+        public void AddToDictionary(Dictionary<String, List<Contact>> dict, string key, Contact contact)
+        {
+            if (dict.ContainsKey(key))
+            {
+                dict[key].Add(contact);
+            }else
+            {
+                dict[key] = new List<Contact>() { contact };
+            }
         }
 
         //Add new contacts and check duplicate if is it exist
@@ -21,6 +37,8 @@ namespace AddressBookSystem
             if (!people.Contains(contact))
             {
                 people.Add(contact);
+                AddToDictionary(contactByCity, contact.City, contact);
+                AddToDictionary(contactByState, contact.State, contact);
                 Console.WriteLine("Add Successfully");
             }
             else
@@ -46,13 +64,14 @@ namespace AddressBookSystem
         public List<Contact> searchPersonByCity(string city)
         {
             return people.Where(p => p.City.Equals(city)).ToList();
-           
+            
         }
 
         //Search person by their State
         public List<Contact> searchPersonByState(string state)
         {
             return people.Where(p => p.State == state).ToList();
+            
         }
     }
 }
