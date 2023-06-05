a91144d23682720085753df070f26602a8a699f2
MukulDevPatel <patelmukuldev@gmail.com>
2023-06-05 21:39:10 +0530

    [Mukul Dev] Add: We are using Json file method for read the data and write in the txt file

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -10,14 +10,15 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using System.Globalization;
+using Newtonsoft.Json;
 
 namespace AddressBookSystem
 {
     public class AddressBookMain 
     {
-        public static void UseCSVHelper()
+        public static void UseJSONFile()
         {
-            string filePath = @"D:\BridgeLabz Second batch\AddressBookSystem\CSVHelperText.txt";
+            string filePath = @"D:\BridgeLabz Second batch\AddressBookSystem\JSONTextfile.txt";
             
             Contact contact1 = new Contact { FirstName = "Jay", LastName = "Kumar", Address = "section A", City = "Vijay Nager", Email = "vikra@gmail.com", PhoneNumber = "6254354813", State = "Mp", Zip = "425136" };
             Contact contact2 = new Contact { FirstName = "Aarav", LastName = "Pratt", Address = "section B2", City = "City5", Email = "ivan@gmail.com", PhoneNumber = "6254354513", State = "UK", Zip = "425125" };
@@ -30,28 +31,20 @@ namespace AddressBookSystem
             //Write to  file:
             List<Contact> contacts = new List<Contact>() { contact1, contact2, contact3, contact4, contact5, contact6 };
 
-            StreamWriter fileWriter = new StreamWriter(filePath);
-            using (CsvWriter csvWriter = new CsvWriter(fileWriter, CultureInfo.InvariantCulture))
-            {
-                csvWriter.WriteHeader<Contact>();
-                csvWriter.NextRecord();
-                csvWriter.WriteRecords<Contact>(contacts);
-            }
+            string jasonString = JsonConvert.SerializeObject(contacts);
+            File.WriteAllText(filePath, jasonString);
 
             //Read to file:
 
             List<Contact> listOfPeople = new List<Contact>();
-            StreamReader fileReader = new StreamReader(filePath);
-            CsvReader csvReader = new CsvReader(fileReader, CultureInfo.InvariantCulture);
 
-            csvReader.Read();
-            Console.WriteLine(csvReader.ReadHeader());
-
-            listOfPeople = csvReader.GetRecords<Contact>().ToList();
-            foreach (Contact contact in listOfPeople)
+            string jsonContent = File.ReadAllText(filePath);
+            listOfPeople = JsonConvert.DeserializeObject<List<Contact>>(jsonContent);
+            foreach (var contact in listOfPeople)
             {
                 Console.WriteLine($"\nFirstName:{contact.FirstName}\tLastName:{contact.LastName}\tAddress:{contact.Address}\tCity:{contact.City}\tState:{contact.State}\tZip:{contact.Zip}\tPhoneNumber:{contact.PhoneNumber}\tEmail:{contact.Email}");
             }
+            
         }
     }
 }
